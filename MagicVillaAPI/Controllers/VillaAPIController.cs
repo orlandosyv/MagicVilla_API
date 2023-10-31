96cac463435a216dd5843e8c14d7fc66334e2539
OrlandoYV <sergioyv@gmail.com>
2023-10-31 00:39:24 -0500

    oy - villa controller

---
+++ MagicVillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,18 @@
+﻿using MagicVillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]    
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> { 
+            new Villa{Id=1, Name= "Pool View" },
+            new Villa{Id=2, Name= "Beach View"}
+            };
+        }
+    }
+}
---
+++ MagicVillaAPI/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVillaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
