d1028c8747cb6088356630763e0c25a8f62735c1
OrlandoYV <sergioyv@gmail.com>
2023-10-31 13:10:02 -0500

    oy - Villa store, Villa DTO, Get Id, 404, 400

--- MagicVillaAPI/Controllers/VillaAPIController.cs
+++ MagicVillaAPI/Controllers/VillaAPIController.cs
@@ -1,18 +1,32 @@
-﻿using MagicVillaAPI.Models;
+﻿using MagicVillaAPI.Data;
+using MagicVillaAPI.Models;
+using MagicVillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVillaAPI.Controllers
 {
-    [Route("api/VillaAPI")]    
+    //[Route("api/VillaAPI")]
+    [Route("api/VillaAPI")] //Do not change it (route)    
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
-        public IEnumerable<Villa> GetVillas()
+        //Http EndPoint
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return new List<Villa> { 
-            new Villa{Id=1, Name= "Pool View" },
-            new Villa{Id=2, Name= "Beach View"}
-            };
+            return Ok(VillaStore.villaList);
+        }
+
+        //[HttpGet] // Get is always predetermined by default (not necessary to put here)
+        [HttpGet("id:int")] 
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0) { return BadRequest(); }
+            var villa = VillaStore.villaList.FirstOrDefault(x => x.Id == id);            
+            if (villa == null) { return NotFound(); }             
+            return Ok(villa);             
         }
     }
+
 }
+
---
+++ MagicVillaAPI/Models/Dto/VillaDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVillaAPI.Models.Dto
+{
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }        
+    }
+}
