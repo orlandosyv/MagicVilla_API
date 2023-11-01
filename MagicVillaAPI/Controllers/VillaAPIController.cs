6132ccc802609d31c858e4579bfbe148afc90767
OrlandoYV <sergioyv@gmail.com>
2023-11-01 00:06:25 -0500

    oy - Post with validations

--- MagicVillaAPI/Controllers/VillaAPIController.cs
+++ MagicVillaAPI/Controllers/VillaAPIController.cs
@@ -12,13 +12,17 @@ namespace MagicVillaAPI.Controllers
     {
         //Http EndPoint
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
             return Ok(VillaStore.villaList);
         }
 
         //[HttpGet] // Get is always predetermined by default (not necessary to put here)
-        [HttpGet("id:int")] 
+        [HttpGet("{id:int}", Name ="GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)] 
+        [ProducesResponseType(StatusCodes.Status400BadRequest)] 
+        [ProducesResponseType(StatusCodes.Status404NotFound)]         
         public ActionResult<VillaDTO> GetVilla(int id)
         {
             if (id == 0) { return BadRequest(); }
@@ -26,6 +30,23 @@ namespace MagicVillaAPI.Controllers
             if (villa == null) { return NotFound(); }             
             return Ok(villa);             
         }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO) 
+        { 
+            if(villaDTO == null) { return BadRequest(villaDTO); }
+            if(villaDTO.Id > 0) { return StatusCode(StatusCodes.Status500InternalServerError); }
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(v => v.Id)
+                          .FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villaDTO);
+            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id},villaDTO);
+        }
+        
+
+
     }
 
 }
--- MagicVillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVillaAPI/Models/Dto/VillaDTO.cs
@@ -1,8 +1,13 @@
-﻿namespace MagicVillaAPI.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVillaAPI.Models.Dto
 {
     public class VillaDTO
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
         public string Name { get; set; }        
     }
 }
